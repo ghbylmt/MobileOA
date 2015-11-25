034db118d3443857ff1f6ac48c6edc99312066c9
DeskTop <1012750881@qq.com>
2015-11-25 17:01:24 +0800

    导航效果测试完成 2015年11月25日17:00:17 BY LMT

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,31 +3,13 @@
  */
 angular.module('starter.controllers', [])
 
-  .controller('setting', function($scope) {})
-  .controller('process_center', function($scope) {})
-  .controller('home_page', function($scope) {})
+  .controller('Setting', function ($scope) {
 
-  .controller('ChatsCtrl', function($scope, Chats) {
-    // With the new view caching in Ionic, Controllers are only called
-    // when they are recreated or on app start, instead of every page change.
-    // To listen for when this page is active (for example, to refresh data),
-    // listen for the $ionicView.enter event:
-    //
-    //$scope.$on('$ionicView.enter', function(e) {
-    //});
-
-    $scope.chats = Chats.all();
-    $scope.remove = function(chat) {
-      Chats.remove(chat);
-    };
   })
+  .controller('ProcessCenter', function ($scope) {
 
-  .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-    $scope.chat = Chats.get($stateParams.chatId);
   })
+  .controller('HomePage', function ($scope) {
 
-  .controller('AccountCtrl', function($scope) {
-    $scope.settings = {
-      enableFriends: true
-    };
   });
+;
