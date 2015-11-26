732481bec40a8f8d4a0305b04f9b6db6832ab2b5
gbylmt <gmbylmt@gmail.com>
2015-11-26 11:24:15 +0800

    成功完成导航菜单栏的设计 接下来开始导航菜单的设计 2015年11月26日11:09:08 By LMT

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,14 +2,9 @@
  * Created by JQLMT on 2015/11/24.
  */
 angular.module('starter.controllers', [])
-
+  .controller('HomePage', function ($scope) {
+  })
   .controller('Setting', function ($scope) {
-
   })
   .controller('ProcessCenter', function ($scope) {
-
-  })
-  .controller('HomePage', function ($scope) {
-
   });
-;
